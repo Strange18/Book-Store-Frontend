a4ec5840325aa7f7d3db4c7638086c0f4bab2d18
Strange18 <prashant.bista.18@gmail.com>
2023-09-15 18:42:01 +0545

    Project Initializex

---
+++ src/components/header.jsx
@@ -0,0 +1,27 @@
+function HeaderComponent() {
+  return (
+    <>
+      <header>
+        <div className="header-logo">Bookey</div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Books</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default HeaderComponent;
